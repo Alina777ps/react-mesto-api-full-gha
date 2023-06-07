ac1beabf985bbcde0804f777be1a9f48188b581e
Alina Tarnakova <alina777pskov@mail.ru>
2023-06-07 10:14:15 +0300

    finish

--- backend/middlewares/limiter.js
+++ backend/middlewares/limiter.js
@@ -1,8 +1,9 @@
 const rateLimit = require('express-rate-limit');
 
-module.exports.limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // максимум 15 минут
-  max: 100, // Ограничение каждого IP до 100 запросов на `окно` (здесь, за 15 минут )
-  standardHeaders: true, // Возвращаем информацию об ограничении скорости в заголовках
-  legacyHeaders: false, // Отключаем заголовки `X-RateLimit-*`
+const limiter = rateLimit({
+  max: 100,
+  windowMS: 55000,
+  message: 'Превышено количество запросов на сервер.',
 });
+
+module.exports = limiter;
