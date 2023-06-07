f024d39cc6f9381e2e6abe0191c98e6ce5d00a38
Alina Tarnakova <alina777pskov@mail.ru>
2023-06-07 14:19:07 +0300

    del like and trash

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -11,12 +11,13 @@ function Card({
   const currentUser = React.useContext(CurrentUserContext);
 
   // Определяем, являемся ли мы владельцем текущей карточки
-  const isOwn = card.owner._id === currentUser._id;
+  //const isOwn = card.owner._id === currentUser._id;
+  const isOwn = (card.owner._id || card.owner) === currentUser._id
   // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
   const isLiked = card.likes.some((i) => i === currentUser._id);
   // Создаём переменную, которую после зададим в `className` для кнопки лайка
   const cardLikeButtonClassName = `element__like-button ${
-    isLiked && "element__like-button_active"
+    isLiked && "element__like-button_active" || ""
   }`;
 
   function handleClick() {
